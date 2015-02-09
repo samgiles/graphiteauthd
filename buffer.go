fdfa2ff366cd6a70bfe6e77cb5bbd1f5684f8d0a
SamGiles <samuel.giles@ft.com>
2015-02-09 23:34:11 +0000

    More efficient implementation (13 allocs/op down to 5 allocs/op)

--- buffer.go
+++ buffer.go
@@ -4,21 +4,31 @@ package main
 // indicating each metric, and byte[] of the remaining parts of the buffer
 func ParseBuffer(buffer []byte) ([][]byte, []byte) {
 	metrics := make([][]byte, 0)
-	metricBuffer := make([]byte, 0xff)
+
+	var metricBufferCapacity uint32 = 0xff
+	metricBuffer := make([]byte, metricBufferCapacity)
 
 	var metricSize uint32 =  0
+	var metricBufferUsage uint32 = 0
 
 	for _, b := range buffer {
 		if b == '\n' {
 
-			newMetric := make([]byte, metricSize)
-			copy(newMetric, metricBuffer[:metricSize])
-
-			metrics = append(metrics, newMetric)
+			metrics = append(metrics, metricBuffer[metricBufferUsage - metricSize:metricBufferUsage])
 			metricSize = 0;
 		} else {
-			metricBuffer[metricSize] = b;
-			metricSize++;
+
+			if metricBufferUsage == metricBufferCapacity {
+				newMetricBufferCapacity := (metricBufferCapacity + 1) * 2
+				newBuffer := make([]byte, metricBufferCapacity, newMetricBufferCapacity)
+				copy(newBuffer, metricBuffer)
+				metricBuffer = newBuffer
+				metricBufferCapacity = newMetricBufferCapacity
+			}
+
+			metricBuffer[metricBufferUsage] = b
+			metricSize++
+			metricBufferUsage++
 		}
 	}
 
